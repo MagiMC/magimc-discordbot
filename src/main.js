99b6def601a31def21143461aa16a780bde68100
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2025-10-18 22:52:54 +0200

    Aggiunto stato di gioco.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, ActivityType } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    client.user.setActivity('MagiMC', { type: ActivityType.Playing });
 });
 
 client.login(process.env.DISCORD_TOKEN).then(() => {
