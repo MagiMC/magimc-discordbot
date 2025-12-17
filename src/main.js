28f5edbff1ffca227ef905c7f069f0c0ad600a75
Umut Hastan <110131246+Clopezio@users.noreply.github.com>
2025-12-18 00:19:58 +0100

    Separato i comandi in cartella commands

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -9,16 +10,28 @@ const client = new Client({
   ]
 });
 
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
   if (message.author.bot) return;
+  if (!message.content.startsWith('!')) return;
 
-  if (message.content === '!ping') {
-    message.reply('Pong!');
-  }
+  const commandName = message.content.slice(1).toLowerCase();
+  const command = client.commands.get(commandName);
+
+  if (!command) return;
+
+  command.execute(message);
 });
 
 client.login(process.env.DISCORD_TOKEN);
